cb20dd47cf87cda9532961b386bd9282d6444fa1
Rocket1184 <rocket1184@outlook.com>
2016-09-19 01:34:57 +0800

    dependency for heroku

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
 const fs = require('fs');
+const url = require('url');
+const path = require('path');
+const http = require('http');
 
 const QQ = require('./src/QQ');
+const ContentType = require('./libs/contentType');
 
 let qq = new QQ();
+let root = path.resolve('.');
+let server = http.createServer((request, response) => {
+    var pathName = url.parse(request.url).pathname;
+    var filePath = path.join(root, pathName);
+    if (request.method === 'GET') {
+        // try to find and read local file
+        fs.stat(filePath, (err, stats) => {
+            // no error occured, read file
+            if (!err && stats.isFile()) {
+                let extName;
+                try {
+                    extName = regexs.extName.exec(pathName)[1];
+                } catch (e) {}
+                response.writeHead(200, { 'content-Type': ContentType.get(extName) });
+                fs.createReadStream(filePath).pipe(response);
+                // cannot find file, but received index request
+            } else {
+                response.writeHead(404, { 'content-Type': 'text/html' });
+                response.end('404NotFound');
+            }
+        });
+    }
+});
+
+var serverPort = process.env.PORT || 8080;
+
+server.listen(serverPort);
 
 qq.Login();
